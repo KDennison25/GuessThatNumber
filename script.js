b4d71db0bb81407fb6da3a7b10a9b75794f6af0a
Student <student@BCCSs-MacBook-Pro.local>
2024-03-11 11:01:53 -0500

    Completed pregame functionality

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ let attempts;
 let guess;
 // For storing the user's response to play again or not play again
 let playAgain;
+
+// Starting alert message
+alert(`Welcome to GUESS THAT NUMBER! Please click OK to start the game`)
+
+// Game restarts as long as the restartGame has a value of true
+while (restartGame){
+    
+    // Asks the user to enter a number to set the  upper bound for the random number that will be created. Also using parseInt to attempt to covert the user's response into a number value.
+    rangeNum = prompt(`Please enter a maximum number for the range:`)
+    rangeNum = parseInt(rangeNum)
+
+    // Verifies the user's entry for a range number is a number greater then ONE (NOTE: All numbers, positive and negative, have a default boolean value of true, except for zero which has a boolean value of false. Also, NaN has a boolean value of false as well.)
+    while (rangeNum <= 1 || !rangeNum){
+        rangeNum = prompt(`Please enter a number greater than 1`)
+        rangeNum = parseInt(rangeNum)
+    }
+
+    // alert(rangeNum)
+
+    // Creates the random number using the range number entered by the user
+    randomNum = Math.floor(Math.random() * rangeNum + 1) + 1
+
+    alert(randomNum)
+
+    break;
+}
